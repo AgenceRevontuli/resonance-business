13d2f2f4adb46f4941a4017728c882d9dfc07d5b
Tristan Tornatore <62868590+AgenceRevontuli@users.noreply.github.com>
2021-05-05 00:21:58 +0200

    fixed untracked files

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'resonance' );
+define( 'DB_NAME', 'resonane' );
 
 /** Utilisateur de la base de données MySQL. */
 define( 'DB_USER', 'root' );
